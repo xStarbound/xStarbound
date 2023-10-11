164f0b2262179fbf87160f3cd7acee059a725089
FezzedOne <RenoFezzedOne@yandex.com>
2023-10-11 11:56:14 -0400

    Added missing files.

---
+++ scripts/linux/setup-qt.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Special setup file for building the Qt GUI stuff only. Needed because these GUIs need to use the system libpng, not the one included with the source.
+
+cd "`dirname \"$0\"`/../..";
+
+rm -rI build/ dist/;
+
+mkdir -p dist;
+cp scripts/linux/sbinit.config dist/;
+
+mkdir -p build;
+cd build;
+
+if [ -d /usr/lib/ccache ]; then
+  export PATH=/usr/lib/ccache/:$PATH;
+fi
+
+LINUX_LIB_DIR=../lib/linux;
+
+cmake \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DSTAR_USE_JEMALLOC=ON \
+  -DCMAKE_INCLUDE_PATH=$LINUX_LIB_DIR/include \
+  -DCMAKE_LIBRARY_PATH=$LINUX_LIB_DIR/ \
+  -DQT_GUIS_ONLY=ON \
+  -DPNG_LIBRARY=/usr/lib/libpng16.so.16 \
+  -DSTAR_BUILD_QT_TOOLS=ON \
+  -DSTAR_ENABLE_STEAM_INTEGRATION=ON \
+  -S ../source/ -B .;
+
+# RelWithAsserts
+
+if [ $# -ne 0 ]; then
+  make -j$*;
+else
+  make;
+fi
+
+cp -r ../dist/* ../test/linux/; # Will replace existing xSB test executables.
---
+++ scripts/windows/setup64.bat
@@ -0,0 +1,48 @@
+cd /d %~dp0
+cd ..\..
+
+mkdir dist
+del dist\*.dll
+copy lib\windows64\*.dll dist\
+copy scripts\windows\sbinit.config dist\
+
+mkdir build
+cd build
+
+if exist "C:\Program Files (x86)\CMake\bin" (
+  set CMAKE_EXE_PATH="C:\Program Files (x86)\CMake\bin"
+) else (
+  set CMAKE_EXE_PATH="C:\Program Files\CMake\bin"
+)
+
+set QT_PREFIX_PATH=C:\Qt\5.6\msvc2015_64
+
+if exist %QT_PREFIX_PATH% (
+
+%CMAKE_EXE_PATH%\cmake.exe ^
+  ..\source ^
+  -G"Visual Studio 17 2022" ^
+  -DSTAR_USE_JEMALLOC=ON ^
+  -DCMAKE_PREFIX_PATH=%QT_PREFIX_PATH% ^
+  -DSTAR_BUILD_QT_TOOLS=ON ^
+  -DSTAR_ENABLE_STEAM_INTEGRATION=ON ^
+  -DSTAR_ENABLE_DISCORD_INTEGRATION=ON ^
+  -DCMAKE_INCLUDE_PATH="..\lib\windows64\include" ^
+  -DCMAKE_LIBRARY_PATH="..\lib\windows64"
+
+) else (
+
+%CMAKE_EXE_PATH%\cmake.exe ^
+  ..\source ^
+  -G "Visual Studio 17 2022" ^
+  -DSTAR_USE_JEMALLOC=ON ^
+  -DSTAR_ENABLE_STEAM_INTEGRATION=ON ^
+  -DSTAR_ENABLE_DISCORD_INTEGRATION=ON ^
+  -DCMAKE_INCLUDE_PATH="..\lib\windows64\include" ^
+  -DCMAKE_LIBRARY_PATH="..\lib\windows64"
+
+)
+
+cd ..
+
+pause
