64908b254ba3934799cee667b3f2dff3c4db0c01
FezzedOne <RenoFezzedOne@yandex.com>
2023-12-07 16:54:21 -0500

    Also changed header.

--- source/core/StarImageProcessing.cpp
+++ source/core/StarImageProcessing.cpp
@@ -32,7 +32,7 @@ Image scaleNearest(Image const& srcImage, Vec2F const& scale) {
 
 // FezzedOne: Need to disable Clang optimisations to ensure proper rendering of custom clothes, which depends on this code working *exactly* as specified.
 #ifdef STAR_COMPILER_CLANG
-Image scaleBilinear(Image const& srcImage, Vec2F const& scale) __attribute__ ((optnone)) {
+__attribute__ ((optnone)) Image scaleBilinear(Image const& srcImage, Vec2F const& scale) {
 #else
 Image scaleBilinear(Image const& srcImage, Vec2F const& scale) {
 #endif
