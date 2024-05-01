f7ab3c6a81e54514ae6666929e67912280b78841
FezzedOne <RenoFezzedOne@yandex.com>
2024-05-01 06:22:56 -0400

    Updated xClient's Linux launch script again.

--- scripts/linux/xclient.sh
+++ scripts/linux/xclient.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Script needed to launch xSB-2 xClient properly. You should be able to double-click this script in your file manager to start xClient.
+sbPath="$(realpath -zL $0 | xargs -0 dirname)/../"; export sbPath;
 cd "`dirname \"$0\"`";
-sbPath="$(readlink -f $0 | xargs -0 dirname)/../"; export sbPath;
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./" ./xclient; #"$0";
 sbPath=; export sbPath;
