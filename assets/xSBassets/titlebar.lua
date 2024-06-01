72e8f36329312323de09833bb301828304f0bdb1
FezzedOne <RenoFezzedOne@yandex.com>
2024-06-01 16:51:54 -0400

    Minor cleanup.

--- assets/xSBassets/titlebar.lua
+++ assets/xSBassets/titlebar.lua
@@ -1,6 +1,6 @@
 -- Post-processing script that overrides any window title changes made by mods.
 
-local postProcPatch = '{"windowTitle": "xClient::FE v' .. xsb.version() .. '"}'
+local postProcPatch = '{"windowTitle": "xClient v' .. xsb.version() .. '"}'
 
 assets.add("/client.config.postproc", postProcPatch)
 assets.patch("/client.config", "/client.config.postproc")
