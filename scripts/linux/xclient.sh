c65557b4b86a37f7e6f17c5acf180339f26db50b
emanueljg <emanueljohnsongodin@gmail.com>
2024-09-12 12:37:18 +0200

    auto-set SDL in sh wrapper

--- scripts/linux/xclient.sh
+++ scripts/linux/xclient.sh
@@ -2,5 +2,13 @@
 # Script needed to launch xClient properly. You should be able to double-click this script in your file manager to start xClient.
 sbPath="$(realpath -zL $0 | xargs -0 dirname)/../"; export sbPath;
 cd "`dirname \"$0\"`";
+
+# automatically switch to wayland support if it seems like it's running
+if [ -z "$WAYLAND_DISPLAY" ]; then 
+  export SDL_VIDEODRIVER="wayland"
+fi
+
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./" ./xclient "$@"; #"$0";
+
+SDL_VIDEODRIVER=; export SDL_VIDEODRIVER;
 sbPath=; export sbPath;
