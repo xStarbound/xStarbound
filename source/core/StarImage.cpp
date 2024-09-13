781db3c11d3a08870c710c34b83b31d4f81db1f4
FezzedOne <RenoFezzedOne@yandex.com>
2024-09-12 21:01:58 -0400

    No, do it Floyd's way.

--- source/core/StarImage.cpp
+++ source/core/StarImage.cpp
@@ -19,7 +19,7 @@ void readPngData(png_structp pngPtr, png_bytep data, png_size_t length) {
 
 bool Image::isPngImage(IODevicePtr device) {
   png_byte header[8];
-  device->readFull((char*)header, sizeof(header));
+  device->readAbsolute(0, (char*)header, sizeof(header));
   if (!png_sig_cmp(header, 0, sizeof(header)))
     return true;
   return false;
