8e70acdd5a39f225846de43bdf3dfbb159af4e20
FezzedOne <RenoFezzedOne@yandex.com>
2024-11-24 18:45:36 -0500

    v3.3.1r2: Fixed Pluto parser compatibility detection for global function calls; this fixes base game fishing rods.

--- source/extern/lua/lparser.cpp
+++ source/extern/lua/lparser.cpp
@@ -207,12 +207,15 @@ static void disablekeyword (LexState *ls, int token) {
 static void check_for_non_portable_code (LexState *ls) {
   if (ls->t.IsNonCompatible() && !ls->t.IsOverridable()) {
     if (ls->getKeywordState(ls->t.token) == KS_ENABLED_BY_PLUTO_UNINFORMED) {
-      if (luaX_lookahead(ls) == '=' || luaX_lookahead(ls) == ':' || luaX_lookahead(ls) == '.' || luaX_lookahead(ls) == '[') { 
-        /* attempting a global assignment or table member access? */
-        disablekeyword(ls, ls->t.token);
-        ls->uninformed_reserved.emplace(ls->t.token, ls->getLineNumber());
-        ls->setKeywordState(ls->t.token, KS_DISABLED_BY_PLUTO_INFORMED);
-        luaX_setpos(ls, luaX_getpos(ls));  /* update ls->t */
+      if (luaX_lookahead(ls) == '=' || luaX_lookahead(ls) == ':' || luaX_lookahead(ls) == '.' ||
+        luaX_lookahead(ls) == '[' || luaX_lookahead(ls) == '(' || luaX_lookahead(ls) == '{') { 
+          /* attempting a global function call, assignment, or table member access? */
+          // FezzedOne: Added compatibility lookahead for function call attempts. Fortunately, none of the incompatible keywords
+          // take an expression as an argument.
+          disablekeyword(ls, ls->t.token);
+          ls->uninformed_reserved.emplace(ls->t.token, ls->getLineNumber());
+          ls->setKeywordState(ls->t.token, KS_DISABLED_BY_PLUTO_INFORMED);
+          luaX_setpos(ls, luaX_getpos(ls));  /* update ls->t */
       }
       else
         ls->setKeywordState(ls->t.token, KS_ENABLED_BY_PLUTO_INFORMED);
