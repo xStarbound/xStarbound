9d8dd3fd8929f37b0d84fbc7bccd67b98e7bb3ad
FezzedOne <RenoFezzedOne@yandex.com>
2024-11-27 16:35:27 -0500

    [FrackinPatch] v1.1: Fixed minor xStarbound compatibility issue in fupower.lua.

--- assets/FezzedOne-FrackinPatch/xsb-frackin-patch.lua
+++ assets/FezzedOne-FrackinPatch/xsb-frackin-patch.lua
@@ -1,25 +1,103 @@
 -- FezzedOne's Frackin' Patch --
-local needsPatch = "/frackinship/scripts/quest/shipupgrades.lua"
+local needsPatch = "/scripts/fupower.lua"
 
 local patchCode = [==[
-function calculateNew(stat, modifier, oldModifier, currentAmount)
-	local info = upgradeConfig[stat]
-	if info then
-		statModifier = math.max(modifier, type(info.min) == "number" and info.min or -math.huge)
-		--sb.logInfo("1. " .. statModifier)
-		statModifier = math.min(statModifier, type(info.max) == "number" and info.max or math.huge)
-		--sb.logInfo("2. " .. statModifier)
-		statNew = math.max(currentAmount + (statModifier - oldModifier), info.totalMin or -math.huge)
-		--sb.logInfo("3. " .. statNew)
-		statNew = math.min(statNew, info.totalMax or math.huge)
-		--sb.logInfo("4. " .. statNew)
-		return statNew, statModifier
-	end
+function power.onNodeConnectionChange(arg, iterations)
+    if power.objectPowerType then
+        local inputCounter = 0
+        local outputCounter = 0
+        if power.objectPowerType == "battery" then return arg end
+        --sb.logInfo("iterations: %s",iterations)
+        iterations = (iterations and iterations + 1) or 1
+        if arg then
+            entitylist = arg
+        else
+            -- FezzedOne: Needs `jarray` because xStarbound's JSON coercion defaults to assuming tables are objects
+            -- and this table is passed back and forth through `world.callScriptedEntity`, which means multiple coercions
+            -- (and conversion of integer keys to strings, breaking iteration in this script).
+            if power.objectPowerType == "battery" then
+                entitylist =
+                    jobject({ battery = jarray({ entity.id() }), output = jarray({}), all = jarray({ entity.id() }) })
+            elseif power.objectPowerType == "output" then
+                entitylist =
+                    jobject({ battery = jarray({}), output = jarray({ entity.id() }), all = jarray({ entity.id() }) })
+            else
+                entitylist = jobject({ battery = jarray({}), output = jarray({}), all = jarray({ entity.id() }) })
+            end
+        end
+        if iterations < 100 then
+            for i = 0, object.inputNodeCount() - 1 do
+                if object.isInputNodeConnected(i) then
+                    local idlist = object.getInputNodeIds(i)
+                    inputCounter = inputCounter + util.tableSize(idlist)
+                    for value in pairs(idlist) do
+                        powertype = callEntity(value, "isPower", iterations)
+                        if powertype then
+                            for j = 1, #entitylist.all + 1 do
+                                if j == #entitylist.all + 1 then
+                                    if powertype == "battery" then
+                                        table.insert(entitylist.battery, value)
+                                    elseif powertype == "output" then
+                                        table.insert(entitylist.output, value)
+                                    end
+                                    table.insert(entitylist.all, value)
+                                    entitylist = (
+                                        callEntity(value, "power.onNodeConnectionChange", entitylist, iterations)
+                                        or entitylist
+                                    )
+                                elseif entitylist.all[j] == value then
+                                    break
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            for i = 0, object.outputNodeCount() - 1 do
+                if object.isOutputNodeConnected(i) then
+                    local idlist = object.getOutputNodeIds(i)
+                    outputCounter = outputCounter + util.tableSize(idlist)
+                    for value in pairs(idlist) do
+                        powertype = callEntity(value, "isPower", iterations)
+                        if powertype then
+                            for j = 1, #entitylist.all + 1 do
+                                if j == #entitylist.all + 1 then
+                                    if powertype == "battery" then
+                                        table.insert(entitylist.battery, value)
+                                    elseif powertype == "output" then
+                                        table.insert(entitylist.output, value)
+                                    end
+                                    table.insert(entitylist.all, value)
+                                    entitylist = (
+                                        callEntity(value, "power.onNodeConnectionChange", entitylist, iterations)
+                                        or entitylist
+                                    )
+                                elseif entitylist.all[j] == value then
+                                    break
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        else
+            sb.logWarn("fupower.lua:power.onNodeConnectionChange: Critical recursion threshhold reached!")
+        end
+        if arg then
+            return entitylist
+        else
+            power.entitylist = entitylist
+            for i = 2, #entitylist.all do
+                callEntity(entitylist.all[i], "updateList", entitylist)
+            end
+        end
+        power.lastInputCount = inputCounter
+        power.lastOutputCount = outputCounter
+    end
 end
 ]==]
 
 if xsb and assets.exists(needsPatch) then
-  local patchedScript = assets.bytes(needsPatch) .. patchCode
-  assets.add(needsPatch, patchedScript)
+    local patchedScript = assets.bytes(needsPatch) .. patchCode
+    assets.add(needsPatch, patchedScript)
 end
-
