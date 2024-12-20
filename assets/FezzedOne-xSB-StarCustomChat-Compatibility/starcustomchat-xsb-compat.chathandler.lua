48ba1f724e5f0670a1e9a549e50cd0e936200fec
FezzedOne <RenoFezzedOne@yandex.com>
2024-12-19 23:56:01 -0500

    [SCC-Compat] Fixed an input.events compatibility issue.

--- assets/FezzedOne-xSB-StarCustomChat-Compatibility/starcustomchat-xsb-compat.chathandler.lua
+++ assets/FezzedOne-xSB-StarCustomChat-Compatibility/starcustomchat-xsb-compat.chathandler.lua
@@ -19,6 +19,28 @@ function init()
         return result
     end
 
+    local base_input_events = input.events
+    input.events = function()
+        local baseEventList = base_input_events()
+        local patchedEventList = jarray()
+        for _, event in ipairs(baseEventList) do
+            if event.type == "MouseWheel" then
+                -- SCC's script expects the `mouseWheel` field to have an integer in it, not a string.
+                local patchedEvent = jobject{
+                    type = "MouseWheel",
+                    data = jobject{
+                        mouseWheel = event.data.mouseWheel == "MouseWheelUp" and 1 or -1,
+                        mousePosition = event.data.mousePosition
+                    }
+                }
+                table.insert(patchedEventList, patchedEvent)
+            else
+                table.insert(patchedEventList, event)
+            end
+        end
+        return patchedEventList
+    end
+
     return base_init()
 end
 
