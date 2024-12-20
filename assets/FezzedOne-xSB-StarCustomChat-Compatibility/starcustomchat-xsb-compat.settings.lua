8777b133d9a0240999fd0b01cbf7efb7a1b2f88d
FezzedOne <RenoFezzedOne@yandex.com>
2024-12-20 02:39:54 -0500

    [SCC-Compat] Baby-proofed imageSize calls in SCC even more.

--- assets/FezzedOne-xSB-StarCustomChat-Compatibility/starcustomchat-xsb-compat.settings.lua
+++ assets/FezzedOne-xSB-StarCustomChat-Compatibility/starcustomchat-xsb-compat.settings.lua
@@ -6,6 +6,9 @@ function init()
     local base_root_imageSize = root.imageSize
     -- Needs to be baby-proofed because SCC doesn't sanitise input or handle exceptions from this callback at all. Ugh.
     root.imageSize = function(imagePath)
+        if imagePath == "" or imagePath:sub(1, 1) ~= "/" then
+            imagePath = "/assetmissing.png" .. imagePath
+        end
         local status, result = pcall(base_root_imageSize, imagePath)
         if status then
             return result
