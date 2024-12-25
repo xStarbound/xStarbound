6d7b595189777b4bc55d5d187eccaf80ef99e2bd
emanueljg <emanueljohnsongodin@gmail.com>
2024-12-25 13:15:54 +0100

    clean up comments

--- nix/fetchFromSteamWorkshop/builder.sh
+++ nix/fetchFromSteamWorkshop/builder.sh
@@ -3,27 +3,14 @@ if [ -e .attrs.sh ]; then source .attrs.sh; fi
 source "${stdenv:?}/setup"
 
 # hack to stop crash
-# ~/.local/share/
 export HOME
 HOME=$(mktemp -d)
 
 mkdir -p "${out:?}"
-
-# export LD_LIBRARY_PATH
-# echo "hi"
-# echo "$LD_LIBRARY_PATH"
 
 steamcmd \
   "+login anonymous" \
   "+workshop_download_item $appId $workshopId" \
   "+quit"
 
-
 mv "$HOME/.local/share/Steam/Steamapps/workshop/content/$appId/$workshopId/"* "$out"
-
-# mv 
-
-# DepotDownloader \
-# 	"${args[@]}" \
-# 	-dir "${out:?}"
-# rm -rf "${out:?}/.DepotDownloader"
