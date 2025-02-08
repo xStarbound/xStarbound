b84f0ff24e3fd7ac165a1cb3f3bba9a13980a1ea
FezzedOne <RenoFezzedOne@yandex.com>
2025-02-08 14:15:05 -0500

    [MyEnternia] Added a necessary type check.

--- assets/FezzedOne-MyEnterniaScriptPatch/xsb-myenternia-patch.lua
+++ assets/FezzedOne-MyEnterniaScriptPatch/xsb-myenternia-patch.lua
@@ -1,7 +1,14 @@
 -- FezzedOne's My Enternia Patch for xStarbound --
 local scriptPath = "/items/buildscripts/alta/object.lua"
 if xsb and assets.exists(scriptPath) then
-  local objectScript = assets.bytes(scriptPath)
-  objectScript = objectScript:gsub('"%%02s"', '"%%2s"')
-  assets.add(scriptPath, objectScript)
+    local objectScript = assets.bytes(scriptPath)
+    objectScript = objectScript:gsub('"%%02s"', '"%%2s"')
+    assets.add(scriptPath, objectScript)
+end
+
+local scriptPath2 = "/items/buildscripts/alta/item.lua"
+if assets.exists(scriptPath2) then
+    local itemScript = assets.bytes(scriptPath2)
+    itemScript = itemScript:gsub("if get%('objectImage'%) then", "if type(get('objectImage')) == 'string' then")
+    assets.add(scriptPath2, itemScript)
 end
