1fbd85e3db4d934b7e8e738ecaa606f1be7ca5cf
FezzedOne <RenoFezzedOne@yandex.com>
2025-02-15 04:22:26 -0500

    [v3.4.2] Ugh, fuck SCC. Removed the patch from the repo.

--- assets/FezzedOne-xSB-StarCustomChat-Compatibility/interface/scripted/starcustomchat/base/chatgui.json.patch.lua
+++
@@ -1,9 +0,0 @@
-function patch(original)
-    if xsb then -- Fixed the chat pane so that Esc opens the escape menu as it should.
-        local patched = original
-        patched.paneLayer = "hud"
-        return patched
-    else
-        return original
-    end
-end
