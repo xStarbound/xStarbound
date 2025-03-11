84c4dd8a3e124dcb76d3cdcc67eb67e16c8e8498
FezzedOne <RenoFezzedOne@yandex.com>
2025-03-11 04:44:02 -0400

    [v3.5.1] Hid stuff that doesn't do anything in the character editor.

--- source/frontend/StarCharCreation.cpp
+++ source/frontend/StarCharCreation.cpp
@@ -186,12 +186,20 @@ CharCreationPane::CharCreationPane(std::function<void(PlayerPtr)> requestCloseFu
   
   if (m_previewPlayer && m_isExistingPlayer) {
     m_oldIdentity = m_previewPlayer->identity();
-    fetchChild<LabelWidget>("lblSkipIntro")->hide();
-    {
-      auto skipIntroButton = fetchChild<ButtonWidget>("btnSkipIntro");
-      skipIntroButton->disable();
-      skipIntroButton->hide();
-    }
+    const StringList itemsToHide = {
+      "lblSkipIntro", 
+      "btnSkipIntro", 
+      "labelShirt", 
+      "shirt",
+      "labelPants", 
+      "pants",
+      "labelShirtColor", 
+      "shirtColor",
+      "labelPantsColor", 
+      "pantsColor",
+    };
+    for (auto& toHide : itemsToHide)
+      fetchChild<Widget>(toHide)->hide();
     setLabel("labelMode", "");
     fetchChild<TextBoxWidget>("name")->setText(m_previewPlayer->name());
     String speciesName = m_previewPlayer->species();
